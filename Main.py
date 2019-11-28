2835909651bf9235f720c8d4ceff89127983dc4c
crosse <crosse@luxurygames.ch>
2019-11-28 10:36:09 +0200

    Add MainGame class and change gitignore

--- Main.py
+++ Main.py
@@ -1,37 +1,6 @@
-import random
-import quest
-import battle
-import time
+import MainGame
 
-#Universal RPG
+# Universal RPG
 
-text = [
-    "Ты двигаешься дальше, но ничего не происходит...",
-    "Ты огляделся, ничего не увидел, и пошёл дальше...",
-    "..."]
-
-print("Начинается игра!")
-isGameEnded = False
-while (not isGameEnded):
-    time.sleep(1)
-    battle_chance = 10
-    quest_chance = 5
-    if random.randint(0, 100) < battle_chance:
-        print("На тебя напал страшный АйМонстр.")
-        print("Начинается битва!")
-        battle.battle_logic()
-        continue
-    elif random.randint(0, 100) < quest_chance:
-        print("Ты обнаружил какое-то строение.")
-        quest_input = input("Обследуешь его? (y|n)")
-        if quest_input == "y" or quest_input == "Y":
-            quest.quest_logic()
-        elif quest_input == "q" or quest_input == "Q":
-            isGameEnded = True
-
-        continue
-    else:
-        print(text[random.randint(0, 2)])
-        continue
-
-print("Game Over!")
+game = MainGame.MainGame()
+game.logic()
---
+++ MainGame.py
@@ -0,0 +1,42 @@
+import random
+import core.quest
+import core.battle
+import time
+
+
+class MainGame:
+    def __init__(self):
+        self.text = [
+            "Ты двигаешься дальше, но ничего не происходит...",
+            "Ты огляделся, ничего не увидел, и пошёл дальше...",
+            "..."]
+        self.isGameEnded = False
+
+    def logic(self):
+        print("Начинается игра!")
+        while not self.isGameEnded:
+            time.sleep(1)
+            battle_chance = 10
+            quest_chance = 5
+            if random.randint(0, 100) < battle_chance:
+                print("На тебя напал страшный АйМонстр.")
+                print("Начинается битва!")
+                battle.battle_logic()
+                continue
+            elif random.randint(0, 100) < quest_chance:
+                print("Ты обнаружил какое-то строение.")
+                quest_input = input("Обследуешь его? (y|n)")
+                if quest_input == "y" or quest_input == "Y":
+                    quest.quest_logic()
+                elif quest_input == "q" or quest_input == "Q":
+                    self.isGameEnded = True
+
+                continue
+            else:
+                print(self.text[random.randint(0, 2)])
+                continue
+
+        print("Game Over!")
+
+
+
